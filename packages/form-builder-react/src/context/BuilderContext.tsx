0938376f89bfa178c8d95646d55cd8ac6e251d73
Parth <parthlad3915@gmail.com>
2026-04-29 09:16:40 -0400

    feat(form-builder-react): wire ExpressionProvider into BuilderProvider

--- packages/form-builder-react/src/context/BuilderContext.tsx
+++ packages/form-builder-react/src/context/BuilderContext.tsx
@@ -25,6 +25,7 @@ const BuilderContext = createContext<BuilderContextValue | null>(null);
 import type { BuilderStorageProvider } from "@buildnbuzz/form-builder-core";
 import { useAutoSave } from "../hooks/use-auto-save";
 import { useBuilderKeyboardShortcuts } from "../hooks/use-keyboard-shortcuts";
+import { ExpressionProvider } from "./ExpressionContext";
 
 // ---------------------------------------------------------------------------
 // Providers
@@ -60,7 +61,9 @@ export const BuilderProvider = ({
     <BuilderContext.Provider value={value}>
       <AutoSaveWiring provider={storageProvider} />
       <KeyboardShortcutWiring />
-      {children}
+      <ExpressionProvider>
+        {children}
+      </ExpressionProvider>
     </BuilderContext.Provider>
   );
 };
